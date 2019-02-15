196d0bc228cf834a9191c3704b606d2f8633e0bf
Henock Tesfaye <henock.tesfaye@ahadoo.com>
2019-02-15 13:46:46 +0300

    Removed get_pictures

--- compress-jpeg.py
+++ compress-jpeg.py
@@ -7,17 +7,6 @@ import os
 from PIL import Image
 
 
-def get_pictures(path):
-    path = os.path.abspath(path)
-    try:
-        for picture in os.listdir(path):
-            absolute_path = os.path.join(path, picture)
-            if os.path.isfile(absolute_path):
-                yield picture
-    except OSError:
-        yield path
-
-
 def compressor_factory(quality, output_directory_prefix='compressed'):
     def inner(picture):
         directory = os.path.dirname(picture)
