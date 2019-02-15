246866acf9936fd115df784c47cd1329741b0f50
Henock Tesfaye <henock.tesfaye@ahadoo.com>
2019-02-15 13:43:33 +0300

    Self-talk in tests

--- tests.py
+++ tests.py
@@ -2,6 +2,23 @@
 # -*- coding: utf-8 -*-
 
 # "What if they send a non image file?" Jupi thought
+# Decisions, decisions. What shoudl Jupi do? Jupi remembers something about
+# reversible failure or some shit. Something about leaving everything not
+# touched if any of them are not correct. "Hmm", Jupi thought. "I'm going to see
+# how cp does it and I'll do whatever it does". So Jupi set out to figure out
+# what cp does. "Good news", Jupi thought. I'm glad I don't have to call
+# Image.open for every image. But Jupi still had questions. What happens if the
+# first argument of cp doesn't exist?. Jupi went out back again to check. Good
+# news again. It just displays the error for each missing file and just keeps
+# going.
+
+# "What if the file doesn't exist?"
+
+# "What if the file doesn't exist?"
+
+
 # "What if the directory they send has images that they want converted, but also
 # miscelaneous files that the user doesn't want converted?"
-# "I think it's better to stick to multiple images instead of
+# "I think it's better to stick to multiple images instead of a directory"
+# "Maybe an output directory should be allowed"
+# "What
